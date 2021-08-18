9328a76abd1a75a9311980e6f00720e9a1d4b48c
7worldclass <7worldclass@gmail.com>
2021-08-18 22:41:47 +0600

    commited

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="home"),
-    path('home/', views.HomeView.as_view(), name="home"),
+    path('', views.HomeView.as_view(), name="index"),
+    path('home/', views.HomeView.as_view(), name="index"),
     path('about/', views.AboutView.as_view(), name="about"),
     path('contract/', views.ContractView.as_view(), name="contract"),
 ]
